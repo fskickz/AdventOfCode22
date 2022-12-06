827936ec00ddf9f1250fce07b127943c8bb5aed0
fskickz <afotball@gmail.com>
2022-12-06 09:55:28 +0100

    Solved in C# as well

---
+++ 6/C#/Program.cs
@@ -0,0 +1,18 @@
+﻿string line = File.ReadAllText("/Users/semreiestad/Documents/AdventOfCode22/6/input.txt").Trim();
+
+
+int Uniqueness(string s, int length)
+{
+    for (int i = length; i < line.Length; i++)
+    {
+        var unique = line.Substring((i - length), length).ToHashSet();
+        if (unique.Count() == length)
+        {
+            return i;
+        }
+    }
+    return 0;
+}
+
+Console.WriteLine(Uniqueness(line, 4));
+Console.WriteLine(Uniqueness(line, 14));
